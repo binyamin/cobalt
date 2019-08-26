f2368b37dc1a21b4f838dce6c3b62bdaadf7f01d
b3u <binyaminagreen@gmail.com>
2019-08-26 12:33:54 -0400

    push to github

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require("gulp");
 
 const sass = require('gulp-sass');
 
+const uglify = require("gulp-uglify");
+const babel = require("gulp-babel");
+const concat = require("gulp-concat");
+
 const rename = require("gulp-rename");
 const sourcemaps = require("gulp-sourcemaps");
 
@@ -17,13 +21,33 @@ function css(cb) {
     .pipe(cleanCSS({ compatibility: "ie8" }))
     .pipe(rename('cobalt.min.css'))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("css"));
+    .pipe(gulp.dest("dist"));
+  cb();
+}
+
+
+function js(cb) {
+  gulp
+    .src("js/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(concat('cobalt.js'))
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(uglify())
+    .pipe(rename("cobalt.min.js" ))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest("dist"));
   cb();
 }
 
 function watch() {
   gulp.watch("sass/**/*.scss", css);
+  // gulp.watch("js/**/*.js", js);
 }
 
-exports.css = css;
+// exports.pack = gulp.parallel(css, js);
+exports.pack = css;
 exports.watch = watch;
