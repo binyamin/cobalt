a5365b32330e0fda4cd7648791b02e398f34b70a
Binyamin Green <binyaminagreen@gmail.com>
2020-03-06 16:36:35 -0500

    update deps

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,6 @@ const gulp = require("gulp");
 
 const sass = require('gulp-sass');
 
-const uglify = require("gulp-uglify");
-const babel = require("gulp-babel");
-const concat = require("gulp-concat");
-
 const rename = require("gulp-rename");
 const sourcemaps = require("gulp-sourcemaps");
 
@@ -21,33 +17,13 @@ function css(cb) {
     .pipe(cleanCSS({ compatibility: "ie8" }))
     .pipe(rename('cobalt.min.css'))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist"));
-  cb();
-}
-
-
-function js(cb) {
-  gulp
-    .src("js/**/*.js")
-    .pipe(sourcemaps.init())
-    .pipe(concat('cobalt.js'))
-    .pipe(
-      babel({
-        presets: ["@babel/env"]
-      })
-    )
-    .pipe(uglify())
-    .pipe(rename("cobalt.min.js" ))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest("css"));
   cb();
 }
 
 function watch() {
   gulp.watch("sass/**/*.scss", css);
-  // gulp.watch("js/**/*.js", js);
 }
 
-// exports.pack = gulp.parallel(css, js);
 exports.pack = css;
 exports.watch = watch;
